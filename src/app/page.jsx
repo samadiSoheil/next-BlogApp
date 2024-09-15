e8788c026eb3f8d90c7cb86a986d54383f0dc2fe
itsSoheil <soheil.samadi79@gmail.com>
2024-09-15 13:14:51 +0330

    Some changes

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className="font-sans font-normal">این یک متن تستی هست </h1>
+      <h1 className="font-sans font-semibold">این یک متن تستی هست </h1>
+      <h1 className="font-sans font-bold">این یک متن تستی هست </h1>
+      <h1 className="font-sans font-black">این یک متن تستی هست </h1>
+    </>
+  );
+}
