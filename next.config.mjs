8626d1efe186d268c30c7d4ffbc6ad2af7fc090c
itsSoheil <soheil.samadi79@gmail.com>
2024-09-17 21:55:13 +0330

    Add new config for remote image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "5000",
+        pathname: "/uploads/coverImage/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
