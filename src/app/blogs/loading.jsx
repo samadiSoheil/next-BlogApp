8c8ebf6a79400a3d8b4e05dac2fb02ab8dd5e0a3
itsSoheil <soheil.samadi79@gmail.com>
2024-09-18 23:59:30 +0330

    Change style

--- src/app/blogs/loading.jsx
+++ src/app/blogs/loading.jsx
@@ -2,7 +2,7 @@ import Spinner from "@/ui/Spinner";
 
 const postLoading = () => {
   return (
-    <div className="w-full h-40 flex flex-col justify-center items-center">
+    <div className="w-full min-h-screen flex flex-col justify-center items-center">
       <p>درحال بارگذاری...</p>
       <Spinner />
     </div>
