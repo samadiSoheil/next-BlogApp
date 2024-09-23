a78f7a295f68b32a3b596bc83a07038d0360d457
itsSoheil <soheil.samadi79@gmail.com>
2024-09-23 11:28:46 +0330

    Add AuthContextProvider and Toaster

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,8 @@
 import "@/styles/globals.css";
 import vazirFont from "@/constants/localFonts";
 import Header from "@/components/Header";
+import { Toaster } from "react-hot-toast";
+import { AuthContextProvider } from "@/context/AuthContextProvider";
 
 export const metadata = {
   title: {
@@ -14,8 +16,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body className={`${vazirFont.variable} font-sans h-[2000px]`}>
-        <Header />
-        <div className="container xl:max-w-screen-xl bg-slate-2000">{children}</div>
+        <AuthContextProvider>
+          <Toaster />
+          <Header />
+          <div className="container xl:max-w-screen-xl bg-slate-2000">{children}</div>
+        </AuthContextProvider>
       </body>
     </html>
   );
