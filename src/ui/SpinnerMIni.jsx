a67752984c61938ed043ca468ddd10a8b3682244
itsSoheil <soheil.samadi79@gmail.com>
2024-09-23 11:05:32 +0330

    Create SpinnerMini component

---
+++ src/ui/SpinnerMIni.jsx
@@ -0,0 +1,9 @@
+const SpinnerMini = () => {
+  return (
+    <div className="w-full flex justify-center items-center">
+      <div className="spinner-mini"></div>
+    </div>
+  );
+};
+
+export default SpinnerMini;
