a58cf7e67d6a9472aca1136f77d821b61951e17a
itsSoheil <soheil.samadi79@gmail.com>
2024-09-23 15:49:21 +0330

    Add rebuild Access-Token logic

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -13,4 +13,28 @@ const http = {
   delete: app.delete,
 };
 
+app.interceptors.request.use(
+  (res) => res,
+  (err) => Promise.reject(err)
+);
+
+app.interceptors.response.use(
+  (res) => res,
+  async (err) => {
+    const originalConfig = err.config;
+    if (err.response.status === 401 && !originalConfig._retry) {
+      originalConfig._retry = true;
+      try {
+        const { data } = await axios.get(
+          `${process.env.NEXT_PUBLIC_BACE_URL}/user/refresh-token`,
+          { withCredentials: true }
+        );
+        if (data) return app(originalConfig);
+      } catch (err) {
+        return Promise.reject(err);
+      }
+    }
+    return Promise.reject(err);
+  }
+);
 export default http;
