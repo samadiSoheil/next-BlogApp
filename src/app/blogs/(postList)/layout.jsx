1defba928a422e6e96e4ec7f7d73c4e92e1b5eab
itsSoheil <soheil.samadi79@gmail.com>
2024-09-25 13:19:35 +0330

    Add Search component to BlogsLayout

--- src/app/blogs/(postList)/layout.jsx
+++ src/app/blogs/(postList)/layout.jsx
@@ -1,14 +1,18 @@
 import { Suspense } from "react";
 import CategoryList from "../_components/CategoryList";
 import Spinner from "@/ui/Spinner";
+import Search from "@/ui/Search";
 
 export default function BlogsLayout({ children }) {
   return (
     <>
       <div className="w-full pb-10 bg-slate-100">
-        <h1 className=" font-black text-3xl md:text-4xl text-secondary-800 py-5 md:py-10">
-          لیست بلاگ ها
-        </h1>
+        <div className="grid grid-cols-1 md:grid-cols-3 items-center">
+          <h1 className=" font-black text-3xl lg:text-4xl text-secondary-700 py-5 md:py-10">
+            لیست بلاگ ها
+          </h1>
+          <Search />
+        </div>
         <div className="w-full grid grid-cols-1 md:grid-cols-12 gap-8 overflow-scrol">
           <div className="w-full col-span-1 md:col-span-12 lg:col-span-3">
             <Suspense fallback={<Spinner />}>
